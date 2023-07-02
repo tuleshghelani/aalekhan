61cacf54906958096742898a2520439e46114b2f
rutvikbhatt <rutikbhatt@83477@gmail.com>
2023-07-02 16:26:45 +0530

    Developed marquee section & contact-us and about-us dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,85 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 // import * as fullpage from 'fullpage.js';
 declare const fullpage: any; // Provide type annotation for fullpage
 import * as AOS from "aos";
-// import Swiper from "swiper";
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormControl, FormGroup } from '@angular/forms';
 
+// import { Swiper } from "swiper";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition(':increment', [
+        style({ transform: 'translateX(0)', transition:'1s' }),
+        animate('400ms ease-in-out', style({ transform: 'translateX(-100%)' }))
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateX(0)', transition:'1s' }),
+        animate('400ms ease-in-out', style({ transform: 'translateX(100%)' }))
+      ]),
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
-  title = 'aalekhan';
-  ngOnInit(){
-    // var video = document.getElementById('bgVideo');
-    //   console.log(video)
-    AOS.init();
-    // var swiper = new Swiper('.blog-slider', {
-    //   spaceBetween: 30,
-    //   effect: 'fade',
-    //   loop: true,
-    //   mousewheel: {
-    //     invert: false,
-    //   },
-    //   // autoHeight: true,
-    //   pagination: {
-    //     el: '.blog-slider__pagination',
-    //     clickable: true,
-    //   }
-    // });
-  }
   
+  contactForm!:FormGroup
+  constructor(){}
+ 
+  carouselItems: any[] = [
+    {image:"../assets/Elon_Musk.webp", name:"Elon Musk", details:"We Understand your brand's purpose, vision , value , audience competition, persona etc. through in-depth discovery session to establish an apt positioning strategy"},
+    {image:"../assets/jeff_bezos.webp", name:"Jeff Bezos", details:"We Understand your brand's purpose, vision , value , audience competition, persona etc. through in-depth discovery session to establish an apt positioning strategy"},
+    {image:"../assets/sundar_pichai.jpeg", name:"Sundar Pichai",details:"We Understand your brand's purpose, vision , value , audience competition, persona etc. through in-depth discovery session to establish an apt positioning strategy"}
+  ];
+  carouselState = 'slideIn';
+  title = 'aalekhan';
+  ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      description: new FormControl()
+    })
+    this.startCarousel();
+    // AOS.init();
+  }
+
+  startCarousel() {
+    setInterval(() => {
+      this.carouselState = 'slideIn';
+      setTimeout(() => {
+        this.carouselItems.push(this.carouselItems.shift());
+        this.carouselState = '';
+      }, 400);
+    }, 3000);
+  }
+
+  previousSlide() {
+    this.carouselState = 'slideIn';
+    setTimeout(() => {
+      this.carouselItems.unshift(this.carouselItems.pop());
+      this.carouselState = '';
+    }, 400);
+  }
+
+  nextSlide() {
+    this.carouselState = 'slideIn';
+    setTimeout(() => {
+      this.carouselItems.push(this.carouselItems.shift());
+      this.carouselState = '';
+    }, 400);
+  }
+
+  submitForm(){
+    console.log('submit data =>',this.contactForm.value)
+    this.contactForm.reset()
+  }
+  closeModel(){
+    this.contactForm.reset()
+  }
+
+  openAboutUs(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServiceComponent } from './service/service.component';
 import { AutoTypeDirective } from './auto-type.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +25,12 @@ import { AutoTypeDirective } from './auto-type.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MnFullpageModule
+    MnFullpageModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
