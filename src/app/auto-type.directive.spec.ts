e7c48621657a6f7b9ac2300ae0be9e0cc8cd0730
tuleshghelani <ghelanitulesh@gmail.com>
2023-08-10 08:09:42 +0530

    Angular site changes

--- src/app/auto-type.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoTypeDirective } from './auto-type.directive';
-
-describe('AutoTypeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoTypeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
