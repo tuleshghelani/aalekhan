ac7a64f16dd97029d9cbf05ba755107076c35f52
tuleshghelani <ghelanitulesh@gmail.com>
2024-02-08 08:51:01 +0530

    Changes in service page

--- src/app/service-dialog/service-dialog.component.ts
+++ src/app/service-dialog/service-dialog.component.ts
@@ -8,10 +8,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ServiceDialogComponent implements OnInit {
 
+  public content: string= '';
+  public serviceName: any;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {
+    this.serviceName=this.data;
     console.log('receive =>>',this.data)
+    console.log('this.data==="Print Media" : ', this.serviceName);
+    
+    if(this.data==="Print Media") {
+      this.content='The project entails the creation of a tri-fold brochure aimed at promoting our new product line of eco-friendly household items. The brochure will serve as a promotional tool distributed during trade shows and events.';
+      this.content+='Develop a design consistent with our brand colors (green, blue, and white) and convey a modern, environmentally conscious theme.';
+    }
   }
 
 }
